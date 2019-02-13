91a2d8845dbb4c408ad1ab4173a60a30160a17bc
Amber Chunn <a.77@me.com>
2019-02-12 16:50:00 -0800

    Modified  to ES5

--- acfk-recipe-block/recipe-block.js
+++ acfk-recipe-block/recipe-block.js
@@ -1,25 +1,8 @@
 /**
- * External dependencies
- */
-import classnames from 'classnames';
-
-/**
- * WordPress dependencies
- */
-const { __, sprintf } = wp.i18n;
-const { 
-	TextControl,
-	TextareaControl,
-} = wp.components;
-const { Fragment } = wp.element;
-const { registerBlockType } = wp.blocks;
-const { } = wp.editor;
-
-/**
  * Register our Block Attributes
  */
 
-export default registerBlockType( 'acfk/recipe-block', {
+wp.blocks.registerBlockType( 'acfk/recipe-block', {
     
     title: 'Recipe',
     icon: 'list-view',
@@ -48,34 +31,11 @@ export default registerBlockType( 'acfk/recipe-block', {
         },
     },
 
-    edit: props => {
-            const { attributes: { prepTime, cookingTime, ingredients, servings }, className, setAttributes } = props;
-        return (
-            <Fragment>
-                <div className={ className }>
-                    <TextControl 
-                        label={ 'Prep Time' }
-                        value={ prepTime }
-                        onChange={ prepTime => setAttributes( { prepTime } ) }
-                    />
-                    <TextControl 
-                        label={ 'Cooking Time' }
-                        value={ cookingTime }
-                        onChange={ cookingTime => setAttributes( { cookingTime } ) }
-                    />
-                    <TextControl 
-                        label={ 'Servings' }
-                        value={ servings }
-                        onChange={ servings => setAttributes( { servings } ) }
-                    />
-                    <TextareaControl
-                        label={ 'Ingredients' }
-                        value={ ingredients }
-                        onChange={ ingredients => setAttributes( { ingredients } ) }
-                    />
-                </div>
-            </Fragment>
-        );
+    edit: function (props) {
+        return wp.element.createElement(
+            wp.element.Fragment, 
+            null,
+         );
     }, // end edit
 
     save() {
