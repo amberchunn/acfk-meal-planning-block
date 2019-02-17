e64484f1717f4ddc3b97bd58eb98cf325e19ca25
fabiankaegy <fabian@arvernus.info>
2019-02-17 15:57:32 +0100

    Change the way attributes are set

--- acfk-recipe-block/recipe-block.js
+++ acfk-recipe-block/recipe-block.js
@@ -2,80 +2,80 @@
  * Register our Block Attributes
  */
 
-wp.blocks.registerBlockType( 'acfk/recipe-block', {
-    
-    title: 'Recipe',
-    icon: 'list-view',
-    description: 'Recipe Card',
-    category: 'common',
-    attributes: {
-        prepTime: {
-            type: 'integer',
-            source: 'meta',
-            meta: 'acfk_prep_time'
-        },
-        cookingTime: {
-            type: 'integer',
-            source: 'meta',
-            meta: 'acfk_cooking_time'
-        },
-        ingredients: {
-            type: 'string',
-            source: 'meta',
-            meta: 'acfk_ingredients'
-        },
-        servings: {
-            type: 'integer',
-            source: 'meta',
-            meta: 'acfk_servings'
-        },
+wp.blocks.registerBlockType("acfk/recipe-block", {
+  title: "Recipe",
+  icon: "list-view",
+  description: "Recipe Card",
+  category: "common",
+  attributes: {
+    prepTime: {
+      type: "string",
+      source: "meta",
+      meta: "acfk_prep_time"
     },
+    cookingTime: {
+      type: "string",
+      source: "meta",
+      meta: "acfk_cooking_time"
+    },
+    ingredients: {
+      type: "string",
+      source: "meta",
+      meta: "acfk_ingredients"
+    },
+    servings: {
+      type: "string",
+      source: "meta",
+      meta: "acfk_servings"
+    }
+  },
+  supports: {
+    inserter: false,
+    multiple: false,
+    reusable: false
+  },
 
-    edit: function (props) {
-        function updateMeta( prepTime, cookingTime, servings, ingredients ) {
-            setAttributes({ prepTime, cookingTime, servings, ingredients });
+  edit: function(props) {
+    return wp.element.createElement(
+      wp.element.Fragment,
+      null,
+      wp.element.createElement(wp.components.TextControl, {
+        label: "Prep Time (min)",
+        className: props.className,
+        value: props.attributes.prepTime,
+        onChange: function(newPrepTime) {
+          props.setAttributes({ prepTime: newPrepTime });
         }
-        
-        return wp.element.createElement(
-            wp.element.Fragment, 
-            null,
-            wp.element.createElement(
-                wp.components.TextControl, {
-                    label: 'Prep Time (min)',
-                    className: props.className,
-                    value: props.attributes.prepTime,
-                    onChange: updateMeta
-                    },
-            ),
-            wp.element.createElement(
-                wp.components.TextControl, {
-                    label: 'Cooking Time (min)',
-                    className: props.className,
-                    value: props.attributes.cookingTime,
-                    onChange: updateMeta
-                    },
-            ),
-            wp.element.createElement(
-                wp.components.TextControl, {
-                    label: 'Servings',
-                    className: props.className,
-                    value: props.attributes.servings,
-                    onChange: updateMeta
-                    },
-            ),
-            wp.element.createElement(
-                wp.components.TextControl, {
-                    label: 'Ingredients',
-                    className: props.className,
-                    value: props.attributes.ingredients,
-                    onChange: updateMeta
-                    },
-            ),
-         );
-    }, // end edit
+      }),
+      wp.element.createElement(wp.components.TextControl, {
+        label: "Cooking Time (min)",
+        className: props.className,
+        value: props.attributes.cookingTime,
+        onChange: function(newCookingTime) {
+          props.setAttributes({ cookingTime: newCookingTime });
+        }
+      }),
+      wp.element.createElement(wp.components.TextControl, {
+        label: "Servings",
+        className: props.className,
+        value: props.attributes.servings,
+        onChange: function(newServings) {
+          props.setAttributes({ servings: newServings });
+        }
+      }),
+      wp.element.createElement(wp.components.TextControl, {
+        label: "Ingredients",
+        className: props.className,
+        value: props.attributes.ingredients,
+        onChange: function(newIngredients) {
+          props.setAttributes({ ingredients: newIngredients });
+        }
+      })
+    );
+  }, // end edit
 
-    save() {
-            // Rendering in PHP
-            return null;
-    },
-} );
+  save() {
+    // Rendering in PHP
+    return null;
+  }
+});
